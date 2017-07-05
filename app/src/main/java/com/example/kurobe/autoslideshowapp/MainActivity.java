c1c9067adecf0afc930e25bfae1c649b938122c7
kurobe <yuuki.kurobe@infodex.co.jp>
2017-07-05 19:28:35 +0900

    スライドショーアプリ

---
+++ app/src/main/java/com/example/kurobe/autoslideshowapp/MainActivity.java
@@ -0,0 +1,175 @@
+package com.example.kurobe.autoslideshowapp;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+
+    Timer mTimer;
+    double mTimerSec = 0.0;
+    int btnFlg = 0;
+
+    Handler mHandler = new Handler();
+
+    Button mNextButton;
+    Button mPrevButton;
+    Button mStButton;
+    Uri imageUris[] = new Uri[3];
+    int nowId = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Android 6.0以降の場合
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // パーミッションの許可状態を確認する
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                // 許可されている
+                getContentsInfo();
+            } else {
+                // 許可されていないので許可ダイアログを表示する
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
+            }
+            // Android 5系以下の場合
+        } else {
+            getContentsInfo();
+        }
+
+
+        mNextButton = (Button) findViewById(R.id.next_btn);
+        mPrevButton = (Button) findViewById(R.id.prev_btn);
+        mStButton = (Button) findViewById(R.id.st_btn);
+
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                nowId++;
+                if(nowId == 3) {
+                    nowId = 0;
+                }
+                imageView.setImageURI(imageUris[nowId]);
+            }
+        });
+
+
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                nowId--;
+                if(nowId < 0) {
+                    nowId = 2;
+                }
+                imageView.setImageURI(imageUris[nowId]);
+            }
+        });
+
+        mStButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (mTimer == null) {
+                    mTimer = new Timer();
+                    mTimer.schedule(new TimerTask() {
+                        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+                        @Override
+                        public void run() {
+
+                            mHandler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    nowId++;
+                                    if (nowId == 3) {
+                                        nowId = 0;
+                                    }
+                                    imageView.setImageURI(imageUris[nowId]);
+                                }
+                            });
+
+                        }
+                    }, 2000, 2000);
+                    mStButton.setText("停止");
+                    mNextButton.setEnabled(false);
+                    mPrevButton.setEnabled(false);
+                } else {
+                    mTimer.cancel();
+                    mTimer = null;
+                    mStButton.setText("再生");
+                    mNextButton.setEnabled(true);
+                    mPrevButton.setEnabled(true);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[]grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CODE :
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContentsInfo();
+                }
+                break;
+            default :
+                break;
+        }
+    }
+
+    private void getContentsInfo() {
+
+        ContentResolver resolver = getContentResolver();
+        Cursor cursor = resolver.query (
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null,
+                null,
+                null,
+                null
+        );
+
+        if (cursor.moveToFirst()) {
+            int i = 0;
+            do {
+                int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                Long id = cursor.getLong(fieldIndex);
+                Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                imageUris[i] = imageUri;
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageURI(imageUris[0]);
+                i++;
+
+
+
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+
+
+    }
+
+}
